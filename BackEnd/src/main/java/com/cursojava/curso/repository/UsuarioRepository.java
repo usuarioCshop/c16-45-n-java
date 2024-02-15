099560866c03534b46017bf306a6d9ce9c904874
RaSantillan <rodrialesanti@hotmail.com>
2024-02-14 21:33:47 -0500

    feat(repository): add UsuarioRepository

---
+++ BackEnd/src/main/java/com/cursojava/curso/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.cursojava.curso.repository;
+
+import com.cursojava.curso.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+}
