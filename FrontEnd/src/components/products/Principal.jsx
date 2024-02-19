d495d4c02fef461614b489a55324b12046ddad48
odv144 <omar.virili@gmail.com>
2024-02-19 17:41:57 -0300

    front semi terminado

--- FrontEnd/src/components/products/Principal.jsx
+++ FrontEnd/src/components/products/Principal.jsx
@@ -3,27 +3,14 @@ import { Container, Heading } from '@chakra-ui/layout'
 
 import ProductsTable from './ProductsTable.jsx'
 import Navbar from '../navbar/Navbar.jsx'
-import ModalWindow from '../ui/ModalWindow.jsx'
+import Header from '../header/Header.jsx'
 
 export const Principal = () => {
   return (
-    <ColorModeProvider>
-    <Container maxW="{[90%, sm, md]}" bgColor="white">
-      <Navbar bgGradient="linear-gradient(180deg, #201F4F 0%, #363583 100%)" />
-      <Heading
-        as="h2"
-        size="3xl"
-        colorScheme="purple63"
-        m={"5"}
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-      >
-        Productos
-        <ModalWindow />
-      </Heading>
+    <Container maxW="{[80%, sm, md]}" h="100vh">
+      <Navbar />
+      <Header />
       <ProductsTable />
     </Container>
-  </ColorModeProvider>
   )
 }
