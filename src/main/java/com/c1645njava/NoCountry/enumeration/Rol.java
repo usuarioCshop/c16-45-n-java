1c031b363812a7a8be8fa63c079b0534f3d1645b
usuariocshop <usuariocshop@gmail.com>
2024-02-21 07:37:21 -0300

    con un proyecto limpio

--- src/main/java/com/c1645njava/NoCountry/enumeration/Rol.java
+++
@@ -1,6 +0,0 @@
-package com.c1645njava.NoCountry.enumeration;
-
-public enum Rol {
-    USER,
-    ADMIN
-}
--- src/main/java/com/example/demo/PruabaApplication.java
+++ src/main/java/com/example/demo/PruabaApplication.java
@@ -1,15 +1,13 @@
-package com.c1645njava.NoCountry;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-
-public class NoCountryApplication {
+public class PruabaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NoCountryApplication.class, args);
+		SpringApplication.run(PruabaApplication.class, args);
 	}
 
 }
