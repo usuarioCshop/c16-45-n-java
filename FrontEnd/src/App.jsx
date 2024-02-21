95152a874f6e53144468eafe4676b4aeca6570f8
Paulo Castillo <xpropollo@gmail.com>
2024-02-21 18:48:47 -0400

    agregando funcionalidad al searchbar y conectando al back

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Route,  Routes } from "react-router-dom";
-import { Login } from "./components/Login/Login.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import { Login } from "./components/Login/Login.jsx";
 import { Container } from "@chakra-ui/react";
 
 import { Principal } from "./components/products/Principal.jsx";
 
-
-
 function App() {
   return (
-
     <BrowserRouter>
-    <Container display="flex" alignContent="center" justifyContent="center" minH="100vh" minW="100vw">
-        
-          <Routes>
-            <Route exact path="/" element={<Login />}></Route>
-            <Route exact path="/productos" element={<Principal/>}></Route>
-            
-          </Routes>
-        
+      <Container
+        display="flex"
+        alignContent="center"
+        justifyContent="center"
+        minH="100vh"
+        minW="100vw"
+      >
+        <Routes>
+          {/* <Route exact path="/" element={<Login />}></Route> */}
+          <Route exact path="/productos" element={<Principal />}></Route>
+        </Routes>
       </Container>
     </BrowserRouter>
-   
   );
 }
 
