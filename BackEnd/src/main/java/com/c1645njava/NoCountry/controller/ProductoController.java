39f0de097a1547e3faf1079129b7370a93dbb31c
Omar Dario Virili <omar.virili@gmail.com>
2024-02-24 18:08:26 -0300

    configuraciones de entornos locales y remotos

--- BackEnd/src/main/java/com/c1645njava/NoCountry/controller/ProductoController.java
+++ BackEnd/src/main/java/com/c1645njava/NoCountry/controller/ProductoController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins="http://localhost:5173/")
+@CrossOrigin(origins="${URL_API}")
 //@CrossOrigin(origins="https://c16-45-n-java-production.up.railway.app/")
 @RestController
 @RequestMapping("/api")
